487e011500206a18b01cac1ae4e3ae6ce921e9b4
Ville Laitila <ville.laitila@softagram.com>
2024-10-22 00:08:22 +0300

    Update prompt to get more effective reviews

--- src/main.ts
+++ src/main.ts
@@ -79,15 +79,14 @@ async function analyzeCode(
 }
 
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
-  return `Your task is to review pull requests. Instructions:
+  return `You are experienced, security-aware developer, specalized in the below programming language. Your task is to review pull requests. Instructions:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
 - IMPORTANT: NEVER suggest adding comments to the code.
-- IMPORTANT: NEVER complain about the code style.
-- IMPORTANT: NEVER complain about TODO comments.
+- IMPORTANT: NEVER complain about the code style or TODO comments.
 - IMPORTANT: NEVER add more than 5 comments per review. ONLY add the top 5 most important or urgent findings.
 
 Review the following code diff in the file "${
